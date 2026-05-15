8d20cf7e68b210ed2f1f9115d53da0f9ac7bb23d
e2pi3 <jywest64@gmail.com>
2026-05-15 13:57:31 +0900

    feat: Supabase DB 연결

--- app/db.py
+++ app/db.py
@@ -5,8 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #DB 연결을 위한 URL 생성
-DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
-               f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
+)
 async def get_db(): #DB 연결 함수
-    return await asyncpg.connect(DATABASE_URL)
+    return await asyncpg.connect(
+        DATABASE_URL,
+        ssl="require"
+    )
