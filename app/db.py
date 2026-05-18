f14c37464b97c7fb5f1d4272b9eb44f1fdf7f058
e2pi3 <jywest64@gmail.com>
2026-05-18 17:10:53 +0900

    feat: db연결 방식 변경 / 최적화 (매 함수 호출마다 연결 -> pool로 서버 실행시 초기 1회 연결, 관리로 변경

--- app/db.py
+++ app/db.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#DB 연결을 위한 URL 생성
 DATABASE_URL = (
     f"postgresql://{os.getenv('DB_USER')}:"
     f"{os.getenv('DB_PASSWORD')}@"
@@ -14,9 +13,24 @@ DATABASE_URL = (
     f"{os.getenv('DB_PORT')}/"
     f"{os.getenv('DB_NAME')}"
 )
-async def get_db(): #DB 연결 함수
-    return await asyncpg.connect(
+
+db_pool: asyncpg.Pool = None
+
+
+async def init_db():
+    global db_pool
+    db_pool = await asyncpg.create_pool(
         DATABASE_URL,
-        ssl="require",
-        statement_cache_size=0
+        min_size=5,
+        max_size=20,
+        statement_cache_size=0,
+        ssl="require"
     )
+
+
+async def close_db():
+    await db_pool.close()
+
+
+def get_pool():
+    return db_pool
