b4df7b9cb7f942e179892bc22e47f1801f98ef24
e2pi3 <jywest64@gmail.com>
2026-06-10 15:36:18 +0900

    feat: public 레포 전환 전작업

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ async def lifespan(app: FastAPI):
     await close_db()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None)
 
 app.include_router(auth_router)
 
